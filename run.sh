5ace72c62672c567d4c8c85702890f0b2c83b6dd
Aleksander Figiel <aleksander.figiel@rtbhouse.com>
2022-04-10 12:21:49 +0200

    run without cloning

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+replace() {
+  URL1="${1/github.com/raw.githubusercontent.com}"
+  URL1="${URL1/\/blob\///}"
+  echo $URL1
+}
 
+echo 'paste Github script URL like https://github.com/feegloo/run-github-script/blob/main/test.sh'
+read URL
+RAW_URL=$(replace $URL)
+curl -s $RAW_URL | sh
